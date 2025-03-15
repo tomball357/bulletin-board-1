0c279759a051a1260bdd8d0b412e55e483167cf1
tomball357 <tball@chicagobooth.edu>
2025-03-15 02:53:12 +0000

    git

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,19 +1,20 @@
 class BoardsController < ApplicationController
-  def index
-    @boards = Board.all
-  end
-
   def show
     @board = Board.find(params[:id])
     @posts = @board.posts # Ensure Board `has_many :posts`
     @post = Post.new # Needed for form
   end
 
+  def index
+    @boards = Board.all
+  end
+
   def create
     @board = Board.new(board_params)
     if @board.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Board created successfully!"
     else
+      flash[:alert] = "Board creation failed."
       render :index
     end
   end
@@ -22,5 +23,6 @@ class BoardsController < ApplicationController
 
   def board_params
     params.require(:board).permit(:name)
+  end
   end
 end
